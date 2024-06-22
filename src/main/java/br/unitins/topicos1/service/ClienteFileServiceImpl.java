3727e237a0c1418ac8c026e0458351a28c761f42
Pedro Viera <126673608+PedroVieira0406@users.noreply.github.com>
2024-06-22 10:06:18 -0300

    Update ClienteFileServiceImpl.java

--- src/main/java/br/unitins/topicos1/service/ClienteFileServiceImpl.java
+++ src/main/java/br/unitins/topicos1/service/ClienteFileServiceImpl.java
@@ -1,84 +1,211 @@
 package br.unitins.topicos1.service;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.jose4j.lang.InvalidAlgorithmException;
+
+import br.unitins.topicos1.dto.ClienteDTO;
+import br.unitins.topicos1.dto.ClienteResponseDTO;
+import br.unitins.topicos1.dto.LoginResponseDTO;
+import br.unitins.topicos1.dto.UpdatePasswordDTO;
+import br.unitins.topicos1.dto.UpdateUsernameDTO;
 import br.unitins.topicos1.model.Cliente;
+import br.unitins.topicos1.model.Login;
 import br.unitins.topicos1.repository.ClienteRepository;
+import br.unitins.topicos1.repository.LoginRepository;
+import br.unitins.topicos1.validation.SenhaIncompativelException;
 import br.unitins.topicos1.validation.ValidationException;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Valid;
+import jakarta.validation.Validator;
+import jakarta.ws.rs.NotFoundException;
 
 @ApplicationScoped
-public class ClienteFileServiceImpl implements FileServiceCliente {
-    // ex = /user/janio/quarkus/images/Cliente
-    private final String PATH_USER = System.getProperty("user.home")
-        + File.separator + "quarkus"
-        + File.separator + "images"
-        + File.separator + "cliente" + File.separator;
+public class ClienteServiceImpl implements ClienteService {
 
     @Inject
-    ClienteRepository ClienteRepository;
- 
+    ClienteRepository clienteRepository;
+
+    @Inject
+    Validator validator;
+
+    @Inject
+    HashService hashService;
+
+    @Inject
+    LoginRepository loginRespository;
+
+    @Inject
+    LoginRepository loginRepository;
+
+    @Override
+    public List<ClienteResponseDTO> getAll() {
+
+        List<Cliente> list = clienteRepository.findAll().list();
+        return list.stream().map(e -> ClienteResponseDTO.valueOf(e)).collect(Collectors.toList());
+    }
+
+    @Override
+    public ClienteResponseDTO findById(Long id) {
+        return ClienteResponseDTO.valueOf(clienteRepository.findById(id));
+    }
+
+    @Override
+    @Transactional
+    public ClienteResponseDTO cadastro(@Valid ClienteDTO clienteDTO) {
+        if(loginRepository.findByUsername(clienteDTO.name())!=null)
+            throw new ValidationException("Error", "Username já utilzado");
+        Login login = new Login();
+        login.setName(clienteDTO.name());
+        // gereando o hash da senha
+        login.setSenha(hashService.getHashSenha(clienteDTO.senha()));
+
+        // salvando o usuario
+        loginRepository.persist(login);
+
+
+        Cliente entity = new Cliente();
+        entity.setNome(clienteDTO.nome());
+        entity.setCpf(clienteDTO.cpf());
+        entity.setEmail(clienteDTO.email());
+        entity.setIdade(clienteDTO.idade());
+        entity.setLogin(login);
+
+        clienteRepository.persist(entity);
+
+        return ClienteResponseDTO.valueOf(entity);
+    }
+
     @Override
     @Transactional
-    public void salvar(Long id, String nomeImagem, byte[] imagem) {
-        Cliente Cliente = ClienteRepository.findById(id);
-        try {
-            Cliente.setNomeImagem(salvarImagem(nomeImagem, imagem));
-        } catch (IOException e) {
-            throw new ValidationException("imagem", e.getMessage());
-        }
+    public ClienteResponseDTO create(@Valid ClienteDTO clienteDTO) {
+        if(loginRepository.findByUsername(clienteDTO.name())!=null)
+            throw new ValidationException("Error", "Username já utilzado");
+        Login login = new Login();
+        login.setName(clienteDTO.name());
+        // gereando o hash da senha
+        login.setSenha(hashService.getHashSenha(clienteDTO.senha()));
+
+        // salvando o usuario
+        loginRepository.persist(login);
+
+
+        Cliente entity = new Cliente();
+        entity.setNome(clienteDTO.nome());
+        entity.setCpf(clienteDTO.cpf());
+        entity.setEmail(clienteDTO.email());
+        entity.setIdade(clienteDTO.idade());
+        entity.setLogin(login);
+
+        clienteRepository.persist(entity);
+
+        return ClienteResponseDTO.valueOf(entity);
     }
 
-    private String salvarImagem(String nomeImagem, byte[] imagem) throws IOException {
-        // verificar o tipo da imagem
-        String mimeType = Files.probeContentType(new File(nomeImagem).toPath());
-        List<String> listMimeType = Arrays.asList("image/jpg", "image/gif", "image/png", "image/jpeg");
-        if (!listMimeType.contains(mimeType)) 
-            throw new IOException("Tipo de imagem não suportado.");
+@Override
+    @Transactional
+    public ClienteResponseDTO update(Long id, ClienteDTO ClienteDTO) {
+        validar(ClienteDTO);
 
-        // verificar o tamanho do arquivo - nao permitir maior que 10mb
-        if (imagem.length > 1024 * 1024 * 10) {
-            throw new IOException("Arquivo muito grande, tamanho máximo 10mb.");
+        Cliente ClienteBanco = clienteRepository.findById(id);
+        if (ClienteBanco == null) {
+            throw new EntityNotFoundException("Cliente não achado com o id " + id);
         }
         
-        // criar pasta quando nao existir
-        File diretorio = new File(PATH_USER);
-        if (!diretorio.exists()) 
-            diretorio.mkdirs();
+        ClienteBanco.setNome(ClienteDTO.nome());
+        ClienteBanco.setEmail(ClienteDTO.email());
+        ClienteBanco.setCpf(ClienteDTO.cpf());
+        ClienteBanco.setIdade(ClienteDTO.idade());
 
-        // gerar nome do arquivo 
-        String nomeArquivo = UUID.randomUUID() 
-                                + "." 
-                                + mimeType.substring(mimeType.lastIndexOf("/") + 1);
-        String path = PATH_USER + nomeArquivo;
+        return ClienteResponseDTO.valueOf(ClienteBanco);
+    }
 
-        // salvar o arquivo
-        File file = new File(path);
-        if (file.exists())
-          throw new IOException("Este arquivo ja existe. Programador, tu deve melhorar esse codigo");
+        @Override
+    @Transactional
+    public void updatePassword(Long id, UpdatePasswordDTO dto) {
 
-        // criar o arquivo no SO
-        file.createNewFile();
-
-        FileOutputStream fos = new FileOutputStream(file);
-        fos.write(imagem);
-        fos.flush();
-        fos.close();
-
-        return nomeArquivo;
+        Cliente cliente = clienteRepository.findById(id);
+        String hashSenhaAntiga = hashService.getHashSenha(dto.oldPassword());
+        if(dto.newPassword() != dto.newPassword2())throw new ValidationException("ERRO", "Senhas nao comprativeis");
+        if (cliente != null) {
+            if (cliente.getLogin().getSenha().equals(hashSenhaAntiga)) {
+                String hashNovaSenha = hashService.getHashSenha(dto.newPassword());
+                cliente.getLogin().setSenha(hashNovaSenha);
+            } else {
+                throw new SenhaIncompativelException("Senha antiga nao corresponde");
+            }
+        } else {
+            throw new NotFoundException();
+        }
     }
 
     @Override
-    public File download(String nomeImagem) {
-       return new File(PATH_USER + nomeImagem);
+    @Transactional
+    public void updateUsername(Long id, UpdateUsernameDTO dto) {
+
+        Cliente cliente = clienteRepository.findById(id);
+        if (cliente != null) {
+            String hashSenhaAntiga = hashService.getHashSenha(dto.password());
+            if(hashSenhaAntiga == cliente.getLogin().getSenha()){
+            cliente.getLogin().setName(dto.newUsername());;
+
+        }else {
+            throw new SenhaIncompativelException("Senha incorreta");
+        }
+        } 
+        else {
+            throw new NotFoundException();
+        }
     }
-    
+
+    public void validarNomeCliente(String nome) {
+        Cliente cliente = clienteRepository.findByNomeCompleto(nome);
+        if (cliente != null)
+            throw  new ValidationException("nome", "O nome '"+nome+"' já existe.");
+    }
+
+    private void validar(ClienteDTO clienteDTO) {
+        Set<ConstraintViolation<ClienteDTO>> violations = validator.validate(clienteDTO);
+        if (!violations.isEmpty())
+            throw new ConstraintViolationException(violations);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        clienteRepository.deleteById(id);
+    }
+
+    @Override
+    public List<ClienteResponseDTO> findByNome(String nome) {
+        List<Cliente> list = clienteRepository.findByNome(nome).list();
+        return list.stream().map(e -> ClienteResponseDTO.valueOf(e)).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ClienteResponseDTO> findByEmail(String email) {
+        List<Cliente> list = clienteRepository.findByEmail(email).list();
+        return list.stream().map(e -> ClienteResponseDTO.valueOf(e)).collect(Collectors.toList());
+    }
+    @Override
+    public long count() {
+        return clienteRepository.count();
+    }
+
+    @Override
+    public long countByNome(String nome) {
+        return clienteRepository.findByNome(nome).count();
+    }
+
+    public LoginResponseDTO login(String name, String senha) {
+        Cliente cliente = clienteRepository.findByUsernameAndSenha(name, senha);
+        return LoginResponseDTO.valueOf(cliente.getLogin());
+    }
 }
